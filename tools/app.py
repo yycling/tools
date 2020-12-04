5143ce281dd448873c1834b2e16ec5debff0e6ae
yy <mumian@webuy.ai>
2020-12-04 18:22:16 +0800

    add log

--- tools/app.py
+++ tools/app.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template
+import logging
+
+logging.basicConfig(filename='/data/logs/tools.log', level=logging.INFO, format='%(asctime)s %(message)s',
+                    datefmt='%Y-%m-%d %X')
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def get_tools():
-    return render_template('./tool.html')
+    logging.info("")
+    return render_template('tool.html')
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",debug=True,port=5000)
+    app.run()
