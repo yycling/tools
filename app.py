8bedd80f50beb38e499b8646916dbba670fae0b1
yy <mumian@webuy.ai>
2020-12-05 16:10:37 +0800

    记录来访问的ip

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import logging
 
 logging.basicConfig(filename='/data/logs/tools.log', level=logging.INFO, format='%(asctime)s %(message)s',
@@ -9,7 +9,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def get_tools():
-    logging.info("")
+    # 记录来访问的ip
+    if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
+        logging.info(request.environ['REMOTE_ADDR'])
+    else:
+        logging.info(request.environ['HTTP_X_FORWARDED_FOR'])
     return render_template('tool.html')
 
 
